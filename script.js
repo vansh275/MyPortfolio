9de6791bf66ce388144907fd17ec7a09af3b7fcf
VanshRajputDev <67416216+vansh275@users.noreply.github.com>
2024-04-27 12:23:21 +0530

    added all files

---
+++ script.js
@@ -0,0 +1,20 @@
+// function resizeFn() {
+//     var imageAlign = document.querySelectorAll('.cardImage');
+//     console.log(imageAlign);
+//     if (window.innerWidth >= 0 && window.innerWidth < 700) {
+//         //alert("mobile");
+//         //imageAlign.classList.add("d-flex");
+//         for (var i = 0; i < imageAlign.length; i++) {
+//             imageAlign[i].classList.add("d-flex");
+//         }
+
+//     }
+//     else if (window.innerWidth >= 700) {
+//         // alert("not mobile");
+//         for (var i = 0; i < imageAlign.length; i++) {
+//             imageAlign[i].classList.remove("d-flex");
+//         }
+//     }
+// }
+// window.onresize = resizeFn;
+// resizeFn();
